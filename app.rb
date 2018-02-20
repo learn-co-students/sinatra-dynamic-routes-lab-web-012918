66a994fb4fdeb3d2a698801d7eab0836b574a714
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-02-20 11:18:22 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,47 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @user = params[:name]
+    "#{@user.reverse}"
+  end
+
+  get '/square/:number' do
+
+    @number = params[:number].to_i
+    "#{@number ** 2}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    string = ""
+    @number.times do
+      string << @phrase
+    end
+    string
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+
+  end
+
+  get '/:operation/:number1/:number2' do
+    operation_hash = {"add" => "+", "subtract" => "-", "multiply" => "*", "divide" => "/"}
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    @result = @number1.method(operation_hash[@operation]).(@number2)
+    "#{@result}"
+
+
+  end
+
 
 end
