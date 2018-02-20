9eba77acb447eff893e59c6c100551777679ed58
github username <github email address>
2018-02-20 15:08:14 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,44 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    @name.reverse
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    square = @number * @number
+    square.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @phrase * @number
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+     params[:word1] +"\s"+ params[:word2] +"\s"+ params[:word3] +"\s"+ params[:word4] +"\s"+ params[:word5] + "."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    if @operation == "divide"
+      num = @number1 / @number2
+      num.to_s
+    elsif @operation == "add"
+      num = @number1 + @number2
+      num.to_s
+    elsif @operation == "multiply"
+      num = @number1 * @number2
+      num.to_s
+    elsif @operation == "subtract"
+      num = @number1 - @number2
+      num.to_s
+    end
+  end
 
 end
